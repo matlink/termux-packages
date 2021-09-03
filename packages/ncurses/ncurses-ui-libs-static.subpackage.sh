b40eeeb9c2a0e8906097041203ca3af02c373e3d
Matlink <matlink@matlink.fr>
2021-09-03 16:49:13 +0200

    Remove unmanaged packages

--- packages/ncurses/ncurses-ui-libs-static.subpackage.sh
+++
@@ -1,2 +0,0 @@
-TERMUX_SUBPKG_INCLUDE="lib/libform*.a lib/libmenu*.a lib/libpanel*.a"
-TERMUX_SUBPKG_DESCRIPTION="Static libraries for terminal user interfaces based on ncurses"
--- packages/ncurses/ncurses-ui-libs.subpackage.sh
+++
@@ -1,2 +0,0 @@
-TERMUX_SUBPKG_INCLUDE="lib/libform*.so* lib/libmenu*.so* lib/libpanel*.so*"
-TERMUX_SUBPKG_DESCRIPTION="Libraries for terminal user interfaces based on ncurses"
